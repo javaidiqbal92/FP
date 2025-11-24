baa8b6289045462cdea938483e0155fa0d607a2e
javaidiqbal92 <javaidiqbalcourse@gmail.com>
2025-11-24 00:31:40 -0800

    app.py

--- app.py
+++ app.py
@@ -82,16 +82,9 @@ class YOLOProcessor(VideoProcessorBase):
 # ---------------------------
 webrtc_streamer(
     key="yolo-live",
-    mode=WebRtcMode.SENDRECV,
+    mode="sendrecv",
     video_processor_factory=YOLOProcessor,
-    media_stream_constraints={
-        "video": {
-            "width": {"ideal": 1280},
-            "height": {"ideal": 720},
-            "facingMode": {"exact": "environment"}  # back camera
-        },
-        "audio": False
-    },
+    media_stream_constraints={"video": True, "audio": False},
 )
 
 # ---------------------------
@@ -112,4 +105,5 @@ if st.session_state.pause and st.session_state.detected_item:
     if col2.button("Ignore"):
         st.session_state.pause = False
         st.session_state.detected_item = None
+
 
