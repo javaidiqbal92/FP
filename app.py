43de5930b971775531d4454cb64501f2fe09a2f9
javaidiqbal92 <javaidiqbalcourse@gmail.com>
2025-11-28 01:40:51 +1000

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+ 
 import streamlit as st
 from ultralytics import YOLO
 import cv2
@@ -23,6 +24,7 @@ CLASS_MAP = {
     6: "Calculator",
 
 }
+# CLASS_MAP = {0:"mobile",}
 HELPING_SET = set(CLASS_MAP.values())
 
 def generate_llm_instructions(dets):
@@ -53,7 +55,7 @@ def yolo_on_frame(bgr):
     scale = new_w / w
     new_h = int(h * scale)
     small = cv2.resize(bgr, (new_w, new_h))
-    results = model.predict(small, conf=0.75, iou=0.45, verbose=False)
+    results = model.predict(small, conf=0.80, iou=0.55, verbose=False)
     res = results[0]
     ann_small = res.plot()
     annotated = cv2.resize(ann_small, (w, h))
@@ -136,8 +138,7 @@ RTC_CONFIGURATION = {
             "username": "openrelayproject",
             "credential": "openrelayproject"
         }
-    ],
-    "iceTransportPolicy": "all",
+    ]
 }
 
 with left:
@@ -290,6 +291,3 @@ if st.session_state.audio_html:
 # ---------- RENDER SUMMARY ----------
 if st.session_state.summary_df is not None:
     box_sum.table(st.session_state.summary_df)
-
-
-
